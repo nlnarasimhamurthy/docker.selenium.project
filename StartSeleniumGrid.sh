0943a40f2b9207409eaee6055483704a1cabae11
nlnarasimhamurthy <nlnarasimhamurthy@gmail.com>
2022-05-16 09:46:04 +0530

    Update StartSeleniumGrid.sh

--- StartSeleniumGrid.sh
+++ StartSeleniumGrid.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Building the containers and running selenium server instance"
-docker-compose -f docker-compose-standalone.yml up
+docker -v
+echo "-------------- Starting Selenium Grid -------------------"
+docker-compose -f docker-compose-standalone.yml up &>/dev/null &
+echo "-------------- Started Selenium Grid --------------------"
